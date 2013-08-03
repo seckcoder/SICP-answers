e66f0e08ffffec3c6c0be4eafe77cfe4d9ae3f6e
seckcoder <seckcoder@Gmail.com>
2013-08-03 17:33:39 +0800

    fix 3.47; Add test-and-set for release

--- chp3/code/47-semaphore-using-test-and-set.scm
+++ chp3/code/47-semaphore-using-test-and-set.scm
@@ -1,42 +1,53 @@
 ;;; 47-semaphore-using-test-and-set.scm
 
 (define (make-semaphore n)
-
+  (let ((cell (list #f)))
     (define (acquire)
-        (if (test-and-set! n)
-            (acquire)
-            'ok))
-    
+      (if (test-and-set! cell)
+        (acquire)
+        (begin
+          (cond ((> n 0)
+                 (set! n (- n 1))
+                 (clear! cell)
+                 #t)
+                (else
+                  (clear! cell)
+                  (acquire))))))
     (define (release)
-        (set! n (+ n 1))
-        'ok)
+      (if (test-and-set! cell)
+        (begin
+          (clear! cell)
+          (release))
+        (begin
+          (set! n (+ n 1))
+          (clear! cell))))
+    (define (self msg)
+      (cond ((eq? msg 'acquire) (acquire))
+            ((eq? msg 'release) (release))
+            (else
+              (error 'semaphore-self "UNKNOWN MESSAGE" msg))))
+    self))
 
-    (define (dispatch mode)
-        (cond ((eq? mode 'acquire)
-                (acquire))
-              ((eq? mode 'release)
-                (release))
-              (else
-                (error "Unknown mode MAKE-SEMAPHORE" mode))))
+; a not practical implementation of test-and-set!
+(define (test-and-set! cell)
+  (if (car cell)
+    #t
+    (begin (set-car! cell #t)
+           #f)))
 
-    dispatch)
-
-(define (test-and-set! n)
-    (if (= n 0)
-        #t
-        (begin (set! n (- n 1))
-               #f)))
+(define (clear! cell)
+  (set-car! cell #f))
 
 #|
 ; 根据注释 174
 ; 以下是一个可以在采用时间片模型的单处理器的 MIT Scheme 里实际运行的 test-and-set!
 
 (define (test-and-set! n)
-    (without-interrupts
-        (lambda ()
-            (if (= n 0)
-                #t
-                (begin (set! n (- n 1))
-                       #f)))))
+  (without-interrupts
+    (lambda ()
+      (if (= n 0)
+        #t
+        (begin (set! n (- n 1))
+               #f)))))
 
 |#
